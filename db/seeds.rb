fcfd389bc6a26f6cc23d3c94c6a67813e3061b16
tvarshith <varsh.wk@gmail.com>
2017-01-18 03:12:09 -0800

    fixed typos for seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,18 +10,18 @@
 	Organization.create(id: org_id, name: "Organization #{org_id}", slug: "Org_#{org_id}")
 end
 
-org_1_hosts = %w('some_host_org1', 'some_host_org2')
-org_2_hosts = %w('org_2_host', 'org_3_host')
+org_1_hosts = %w(some_host_org1 some_host_org2)
+org_2_hosts = %w(org_2_host org_3_host)
 
 
 
 
-Event.create(organization_id: 1, host_name: sample(org_1_hosts), action: 'new')
-Event.create(organization_id: 1, host_name: sample(org_1_hosts), action: 'new1')
-Event.create(organization_id: 1, host_name: sample(org_1_hosts), action: 'new2')
-Event.create(organization_id: 2, host_name: sample(org_1_hosts), action: 'new3')
-Event.create(organization_id: 2, host_name: sample(org_1_hosts), action: 'new4')
-Event.create(organization_id: 2, host_name: sample(org_1_hosts), action: 'new5')
-Event.create(organization_id: 3, host_name: sample(org_1_hosts), action: 'new6')
-Event.create(organization_id: 4, host_name: sample(org_2_hosts), action: 'new7')
-Event.create(organization_id: 5, host_name: sample(org_2_hosts), action: 'new8')
+Event.create(organization_id: 1, host_name: org_1_hosts.sample, action: 'new')
+Event.create(organization_id: 1, host_name: org_1_hosts.sample, action: 'new1')
+Event.create(organization_id: 1, host_name: org_1_hosts.sample, action: 'new2')
+Event.create(organization_id: 2, host_name: org_1_hosts.sample, action: 'new3')
+Event.create(organization_id: 2, host_name: org_1_hosts.sample, action: 'new4')
+Event.create(organization_id: 2, host_name: org_1_hosts.sample, action: 'new5')
+Event.create(organization_id: 3, host_name: org_1_hosts.sample, action: 'new6')
+Event.create(organization_id: 4, host_name: org_2_hosts.sample, action: 'new7')
+Event.create(organization_id: 5, host_name: org_2_hosts.sample, action: 'new8')
